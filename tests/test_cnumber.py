85fb0703cf74ecf6a105426f1fe84710dae47b2d
Alex Mboutchouang Ghomsi <mboutchouangalex@gmail.com>
2022-09-28 23:50:48 +0200

    fix pylint

--- tests/test_cnumber.py
+++ tests/test_cnumber.py
@@ -1,18 +1,31 @@
-from cnumber import ComplexNumber
+"""Test Module"""
 import unittest
+
+from cnumber import ComplexNumber
 
 
 class TestSum(unittest.TestCase):
-
+    """Testing class for cnumber."""
     def test_sum(self):
-        a = ComplexNumber(1,2)
-        b = ComplexNumber(3,4)
-        self.assertEqual(a+b, ComplexNumber(4,6), "Should be 4 +6i")
+        """Test the summe."""
+        number1 = ComplexNumber(1, 2)
+        number2 = ComplexNumber(3, 4)
+        self.assertEqual(
+            number1 + number2,
+            ComplexNumber(4,6),
+            "Should be 4 + 6i"
+        )
 
     def test_mutiply(self):
-        a = ComplexNumber(1,2)
-        b = ComplexNumber(3,4)
-        self.assertEqual(a*b, ComplexNumber(-5,10), "Should be 6")
+        """Test the multiplication."""
+        number1 = ComplexNumber(1 ,2)
+        number2 = ComplexNumber(3, 4)
+        self.assertEqual(
+            number1 * number2,
+            ComplexNumber(-5,10),
+            "Should be 6!"
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
